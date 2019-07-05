c5abb88e7b4ae7320fa60db42a7a892b8f12be5b
Yogesh Lokare <yogeshlokare7@gmail.com>
2019-07-05 14:35:17 +0530

    Added delete function,edit fuction to dailystaff

--- src/app/services/daily-staff.service.ts
+++ src/app/services/daily-staff.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { RestApi } from '../models/api/rest-api';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { DailyStaff } from '../models/daily-staff';
 
 
@@ -9,7 +9,10 @@ import { DailyStaff } from '../models/daily-staff';
   providedIn: 'root'
 })
 export class DailyStaffService {
+  
   api  = new RestApi();
+  toasteService: any;
+  toasterService: any;
   constructor(private httpClient:HttpClient) { }
 
   getDailyStaffList() : Observable< DailyStaff[]>{
@@ -25,9 +28,16 @@ export class DailyStaffService {
     return this.httpClient.post< DailyStaff>(this.api.DAILYSTAFF_LIST, dailystaff);
   }
 
-  deleteDailyStaff(id:number) : Observable<any>{
-    const url = `${this.api.DAILYSTAFF_LIST}/${id}`;
-    return this.httpClient.delete(url);
+  deleteDailyStaff(id: number): void {
+    console.log(id);
+    const url = `${this.api.DAILYSTAFF_LIST}/${id}`
+    this.httpClient.delete(url).subscribe(data => {
+      //this.toasteService.openSuccessSnackBar('Successfully deleted', 'OK', 1000);
+    },
+      (err: HttpErrorResponse) => {
+        //this.toasterService.showToaster('Error occurred. Details: ' + err.name + ' ' + err.message, 8000);
+      }
+    );
   }
 
   uploadImage(id:number, selectedFile:File):Observable<any>{
--- src/app/views/company/company-user/company-user.component.ts
+++ src/app/views/company/company-user/company-user.component.ts
@@ -56,6 +56,7 @@ export class CompanyUserComponent implements OnInit {
   }
 
   view(id: number) {
+    
 
   }
   deleteUser(id: number) {
